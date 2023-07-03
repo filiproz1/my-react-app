c85d93820e70a7ca28ae3111a8c5f158a011d9ff
filiproz1 <filiproz@student.agh.edu.pl>
2023-07-03 21:10:29 +0200

    asdasd

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
 asdasdasdasd
-
+edited editted
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
